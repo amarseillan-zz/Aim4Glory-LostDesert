bf757d9f7ad5495133fe87d80a2099b8635ac464
Pablo Marseillan <pablomarseillan@Pablos-iMac.local>
2013-12-01 12:41:19 -0300

    Initial Commit

---
+++ AimForGlory: The lost desert/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  AimForGlory: The lost desert
+//
+//  Created by Pablo Marseillan on 12/1/13.
+//  Copyright amarseillan 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ AimForGlory: The lost desert/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AimForGlory: The lost desert
+//
+//  Created by Pablo Marseillan on 12/1/13.
+//  Copyright amarseillan 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
